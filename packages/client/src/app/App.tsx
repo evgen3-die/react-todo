36dd3d41f08fb95f63d044127f88a270018495e2
Evgenii Kuznetsov <harsel1234@gmail.com>
2020-04-03 17:09:55 +0400

    Установлен и подключен antd

---
+++ packages/client/src/app/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Layout } from 'antd';
+import './App.css';
+
+const { Content } = Layout;
+
+function App() {
+  return (
+    <Layout className="App">
+      <Content>
+        app
+      </Content>
+    </Layout>
+  );
+}
+
+export default App;
