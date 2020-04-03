9e61ceee1aa309bf1bb8feaf86b323093d36f2db
Evgenii Kuznetsov <harsel1234@gmail.com>
2020-04-03 21:16:12 +0400

    Включен hmr

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -4,10 +4,15 @@ import 'antd/dist/antd.css';
 import './index.css';
 import App from './app';
 
-ReactDOM.render(
+const render = () => ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+render();
+
+if (module.hot) {
+  module.hot.accept('./app', render)
+}
